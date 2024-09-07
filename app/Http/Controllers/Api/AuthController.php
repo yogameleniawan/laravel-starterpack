62c8ac697f8ccface3c183f329e7abd04fb09433
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-09-07 19:50:33 +0700

    feat(auth): add auth sanctum api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->only('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $token = Auth::user()->createToken('authToken')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out'], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/login', function () {
+        return response()->json(['message' => 'Please Login [POST] /api/auth/login'], 401);
+    })->name('login');
+    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register'])->name('register');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->name('logout');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
